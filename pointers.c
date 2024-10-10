b242fa37de2d200dd53478fd9d52c749a69cbc06
vis <gtfovis@gmail.com>
2024-10-10 20:45:28 +0300

    pp

--- pointers.c
+++ pointers.c
@@ -26,7 +26,20 @@ int main(int argc, char const *argv[])
     create_kingdoms(&Caledor);
     printf("%s\n", Caledor.nation);
 
-    // 
+    // simple pointer to variable
+    int q = 1;
+    int *pQ = &q;
+    printf("%d\n", *pQ);
+
+    // pointer to array
+    int arr[3] = {1, 5, 7};
+    int *pArr = arr; //not need & in array pointer
+    printf("%d\n", *(arr + 0)); // 1 'cause 1 is 0 element of array, also need () after *
+
+    int g = 101;
+    int *pG = &g;
+    int **pGG = &pG; // pointer to pointer
+    printf("%d\n", **pGG); // **
 
 
 
