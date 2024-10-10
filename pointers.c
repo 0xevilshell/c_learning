a72b8f79f08cb238bedcbb7d39ae6322d853f738
vis <gtfovis@gmail.com>
2024-10-10 21:14:12 +0300

    pp2

--- pointers.c
+++ pointers.c
@@ -36,12 +36,24 @@ int main(int argc, char const *argv[])
     int *pArr = arr; //not need & in array pointer
     printf("%d\n", *(arr + 0)); // 1 'cause 1 is 0 element of array, also need () after *
 
+    // pointer to pointer
     int g = 101;
     int *pG = &g;
     int **pGG = &pG; // pointer to pointer
     printf("%d\n", **pGG); // **
 
+    // pointer to function
+    int lord_attack(int range, int damage) {
+        return range * damage;
+    }
+    /* pointer to attack funtion */ 
+    int (*attack_ptr)(int, int) = &lord_attack; // pointer must have type!!!
+    printf("Lords attack is %d\n", attack_ptr(52, 25));
 
+    // pointer to array of pointers
+    int a = 5, b = 10, c = 15;
+    int *array_ptr[3] = {&a, &b, &c}; // array has [3] elements
+    printf("array has %d element \n", *array_ptr[0]); // array must have *
 
     return 0;
 }
