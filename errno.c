c35d7cc51cb91b9884adf356741fa30b95649a61
vis <gtfovis@gmail.com>
2024-10-27 15:03:10 +0300

    some comments

--- errno.c
+++ errno.c
@@ -3,7 +3,8 @@
 #include <errno.h>  // Для errno
 #include <string.h> // Для strerror()
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) { //c - count of program arguments. first (1) is a program itself
+// v - number of arguments from 0 to n.. 0 - program itself, 1 - first agrument. example ./myprog -f, -f is a first agrument
 
     if (argc != 2) {
         printf("Usage: %s needs a <filename> monka!\n", argv[0]);
